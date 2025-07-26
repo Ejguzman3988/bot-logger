57eb8645feb839d8df2e498be67cb9ebbbf5504f
Eriberto Guzman <eriberto.guzman@outlook.com>
2025-07-26 14:31:49 -0400

    added dependencies and uv.lock

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+import shutil
+
+"""Check if tmux is installed and available inthe system's Path. """
+def is_tmux_installed():
+    return shutil.which("tmux")
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
